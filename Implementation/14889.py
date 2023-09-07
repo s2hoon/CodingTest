b16bc0cca6b05e8b1e1c460423bd26c2aa86613e
suhoon <jobak4200nim@nate.com>
2023-09-07 23:29:43 +0900

    14889

--- Implementation/14889.py
+++ Implementation/14889.py
@@ -1,27 +1,35 @@
 import sys
+from itertools import permutations
+from itertools import combinations
 N = int(sys.stdin.readline().strip())
 person = [ i for i in range(1,N+1)]
-result = []
 graph = []
-last = []
+result = 1e9
+
 for i in range(N):
     graph.append(list(map(int, sys.stdin.readline().strip().split())))
     
-def BackTrack(start,cnt):
+for comb in combinations(person, N//2):
+    S = 0
+    comb = list(comb)
+    for t in combinations(comb,2):
+        (a, b) = t
+        S += graph[a-1][b-1] 
+        S += graph[b-1][a-1]
+    #나머지 temp 에 추가
+    temp = []
+    for item in person:
+        if item not in comb:
+            temp.append(item)
+    S2 = 0
+    for t in combinations(temp,2):
+        (a, b) = t
+        S2 += graph[a-1][b-1] 
+        S2 += graph[b-1][a-1]
 
-    if cnt == N //2 :
-        last.append(sum(result))
-        return
-    
+    result = min(abs(S-S2),result)
 
-    for i in range(start,N):
-        result.append(i)
-        BackTrack(i+1,cnt+1)
-        result.pop()
-        
-BackTrack(1,0)
-print(last)
-print(graph)
+print(result)
 
 
 
