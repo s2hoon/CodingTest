c894ee834dc39003b74ba603213b15f66f1a83dd
suhoon <jobak4200nim@nate.com>
2023-11-13 00:30:46 +0900

    java

--- JavaCodingTest/src/Main.java
+++ JavaCodingTest/src/Main.java
@@ -1,26 +1,6 @@
-import java.util.*;
-import java.io.*;
-import java.util.stream.Collectors;
-
 public class Main {
- 
-
-
-    public static void main(String[] args) throws Exception {
-
-            int [] numbers = {1,2,3,4,5};
-
-            List<Integer> l = new ArrayList<>(Arrays.stream(numbers).boxed().collect(Collectors.toList()));
-
-            System.out.println(Collections.max(l));
-
-            
-        }
-      
-
-
-
     
-
-
+    public static void main(String[] args) {
+        
+    }
 }
